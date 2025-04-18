b8794c6d43ba34c503de01ccfc2f95cdb76e31d8
Lucas Román Fernandez <lukychillcontacto@gmail.com>
2025-04-18 14:44:06 -0300

    calendar ready

--- public/calendario.js
+++ public/calendario.js
@@ -3,31 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
   
     const calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'timeGridWeek',
+      locale: 'es',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
-        right: 'timeGridWeek,timeGridDay'
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      slotMinTime: "09:00:00",
-      slotMaxTime: "20:00:00",
-      allDaySlot: false,
-      locale: 'es',
       events: [
         {
           title: 'Disponible',
-          daysOfWeek: [1, 3, 5], // Lunes, Miércoles y Viernes
+          daysOfWeek: [1, 3, 5],
           startTime: '15:00',
           endTime: '17:00',
           display: 'background',
           color: '#90ee90'
-        },
-        {
-          title: 'Disponible',
-          daysOfWeek: [2, 4], // Martes y Jueves
-          startTime: '10:00',
-          endTime: '12:00',
-          display: 'background',
-          color: '#add8e6'
         }
       ]
     });
