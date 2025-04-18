2cbf8d535865c4713ac257b9de0534c61084eb0d
Lucas Román Fernandez <lukychillcontacto@gmail.com>
2025-04-18 12:38:18 -0300

    change docs to public, server.js add a redirection to index.

--- server.js
+++ server.js
@@ -26,3 +26,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
+
+// Redirección a index.html para rutas no API (🚨 esto va último)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
